05de553dadbc48a8ed8578476153750ff3184e11
ReisenLOL <157349179+ReisenLOL@users.noreply.github.com>
2025-04-20 23:14:19 -0700

    funny landmine

--- Assets/Scripts/PlayerWeapons/Bear Trap/Landmine.cs
+++ Assets/Scripts/PlayerWeapons/Bear Trap/Landmine.cs
@@ -1,16 +1,36 @@
+using Bremsengine;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Landmine : Weapon
 {
+    [SerializeField] LayerMask unitCollectionLayer;
+    [SerializeField] float explosionRadius;
     private void Start()
     {
+        if (EnemyUnit.TryGetRandomAliveEnemy(out EnemyUnit a))
+        {
+            RotateToTarget(a.CurrentPosition);
+        }
         SetWeaponProperties();
+        explosionRadius = weaponLevelData.specialPropertyA;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         HitPacket packet = new(transform.position, damage);
         if (TryHitOther(packet, collision))
         {
+            GeneralManager.FunnyExplosion(transform.position, explosionRadius * 0.5f);
+            EnemyUnit originalHitUnit = collision.GetComponent<EnemyUnit>();
+            if (EnemyUnit.TryFindInCircleCast(transform.position, explosionRadius, unitCollectionLayer, out HashSet<EnemyUnit> explodedUnits))
+            {
+                foreach (EnemyUnit item in explodedUnits)
+                {
+                    if (item == originalHitUnit) { continue; }
+                    packet.HitPosition = item.CurrentPosition;
+                    item.PerformHit(packet);
+                }
+            }
             Destroy(gameObject);
         }
     }
